cbb4488f6f4b9a51728de444567f31c66d5dbbb5
based4real <45218699+based4real@users.noreply.github.com>
2023-12-04 21:14:51 +0100

    små UI ændringer til mediaplayer baren

--- src/mytunes/GUI/Controller/ny/MediaPlayerController.java
+++ src/mytunes/GUI/Controller/ny/MediaPlayerController.java
@@ -1,4 +1,26 @@
 package mytunes.GUI.Controller.ny;
 
-public class MediaPlayerController {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.StackPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MediaPlayerController implements Initializable {
+
+    public Slider sliderAudio;
+    public Slider sliderTime;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        sliderDesign();
+    }
+
+    public void sliderDesign() {
+
+
+    }
 }
