a2b0bd2b9bea88f674f62e19c852a30a769e6f13
based4real <45218699+based4real@users.noreply.github.com>
2023-12-05 00:51:35 +0100

    flere ændringer til mediaplayer bar

--- src/mytunes/GUI/Controller/ny/MediaPlayerController.java
+++ src/mytunes/GUI/Controller/ny/MediaPlayerController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Slider;
 import javafx.scene.layout.StackPane;
 
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class MediaPlayerController implements Initializable {
@@ -21,6 +22,22 @@ public class MediaPlayerController implements Initializable {
 
     public void sliderDesign() {
 
+        sliderTime.valueProperty().addListener((obs, oldValue, newValue) -> {
+            double percentage = 100.0 * newValue.doubleValue() / sliderTime.getMax();
+            String style = String.format(Locale.US,
+                    // in the String format,
+                    // %1$.1f%% gives the first format argument ("1$"),
+                    // i.e. percentage, formatted to 1 decimal place (".1f").
+                    // Note literal % signs must be escaped ("%%")
+
+                    "-track-color: linear-gradient(to right, " +
+                            "-fx-accent 0%%, " +
+                            "-fx-accent %1$.1f%%, " +
+                            "-default-track-color %1$.1f%%, " +
+                            "-default-track-color 100%%);",
+                    percentage);
+            sliderTime.setStyle(style);
+        });
 
     }
 }
